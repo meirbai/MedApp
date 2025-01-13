064b62e78c0d626beb7db0c87db776f447ef79fa
meirbai <123232231+meirbai@users.noreply.github.com>
2025-01-13 18:04:52 +0500

    Add files via upload

---
+++ MedApp/MedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedAppApp.swift
+//  MedApp
+//
+//  Created by Bissengali on 09.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct MedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
